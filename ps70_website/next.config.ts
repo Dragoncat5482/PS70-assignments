d575c0f18088a157845bb3369fd4f868c3a9231b
Dragoncat5482 <aliceleleliu@gmail.com>
2026-01-28 17:41:29 -0500

    More deploy stuff

--- ps70_website/next.config.ts
+++ ps70_website/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/PS70-assignments',
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
